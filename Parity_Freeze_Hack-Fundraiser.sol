feaf6117727d2b4df69c9e7db5ad4503571f0385
Dustin Gearhart <gearhart26@gmail.com>
2021-08-18 09:46:30 -0700

    Update Parity_Freeze_Hack-Fundraiser.sol

--- Parity_Freeze_Hack-Fundraiser.sol
+++ Parity_Freeze_Hack-Fundraiser.sol
@@ -1,1 +1,6 @@
+// PARITY FREEZE HACK CODE EXAMPLE //
 
+// Reused some fundraiser code from DAO example to illistrate the problem with the Parity Freeze Hack
+    
+    pragma solidity ^0.4.8;
+    import "./Parity_Freeze_Hack_Library.sol";

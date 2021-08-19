5805a322d05ef307ad43a4186c28b81c53fd4e9e
Dustin Gearhart <gearhart26@gmail.com>
2021-08-19 01:22:06 -0700

    Update Parity_Freeze_Hack-Fundraiser.sol

--- Parity_Freeze_Hack-Fundraiser.sol
+++ Parity_Freeze_Hack-Fundraiser.sol
@@ -4,3 +4,29 @@
     
     pragma solidity ^0.4.8;
     import "./Parity_Freeze_Hack_Library.sol";
+
+    contract Fundraiser{
+    
+        Library lib = Library(0xbbf289d846208c16edc8474705c748aff07732db);
+    
+        mapping  (address => uint) balances;
+    
+        function contribute() payable{
+            balances[msg.sender] += msg.value;
+        }
+    
+        function withdraw(){
+            //if(balances[msg.sender] == 0)
+            if(lib.isNotPositive(balances[msg.sender])){
+                throw;
+            }
+        
+            balances[msg.sender] = 0;
+            msg.sender.call.value(balances[msg.sender])();
+        }
+        
+        function getFunds() returns (uint){
+            return address(this).balance;
+        }
+        
+    }
